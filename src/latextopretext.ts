edcb81bf995221484d9fb6bbd2019c3b97802c05
Oscar Levin <oscar.levin@unco.edu>
2023-11-27 08:06:03 -0700

    format

--- src/latextopretext.ts
+++ src/latextopretext.ts
@@ -1,26 +1,26 @@
 // module
 
-const BLOCKS: {[key: string]: string } = {
-  "axiom": "axiom",
-  "theorem": "theorem",
-  "lemma": "lemma",
-  "corollary": "corollary",
-  "proposition": "proposition",
-  "definition": "definition",
-  "example": "example",
-  "remark": "remark",
-  "exercise": "exercise",
-  "question": "question",
-  "problem": "problem",
-  "solution": "solution",
-  "proof": "proof",
-  "note": "note",
-  "aside": "aside",
-  "figure": "figure",
-  "table": "table",
-  "algorithm": "algorithm",
-  "enumerate": "ol",
-  "itemize": "ul",
+const BLOCKS: { [key: string]: string } = {
+  axiom: "axiom",
+  theorem: "theorem",
+  lemma: "lemma",
+  corollary: "corollary",
+  proposition: "proposition",
+  definition: "definition",
+  example: "example",
+  remark: "remark",
+  exercise: "exercise",
+  question: "question",
+  problem: "problem",
+  solution: "solution",
+  proof: "proof",
+  note: "note",
+  aside: "aside",
+  figure: "figure",
+  table: "table",
+  algorithm: "algorithm",
+  enumerate: "ol",
+  itemize: "ul",
 };
 
 // Paragraph element conversions /////////////////////////////////
@@ -83,8 +83,8 @@ function convertBS(text: string) {
     // console.log("TEXT: "+text);
     // Item:
     if (command[1] === "\\item") {
-      return "<li>\n\t<p>\n\t\t" + convertP(text) + "\n<\p>\n</li>\n";
-    } 
+      return "<li>\n\t<p>\n\t\t" + convertP(text) + "\n<p>\n</li>\n";
+    }
     // \vspace or \vskip:
     else if (["\\vskip", "\\vspace"].includes(command[1].trim())) {
       // console.log("VSPACE");
@@ -92,7 +92,9 @@ function convertBS(text: string) {
     }
     // All other cases, just leave a comment:
     else {
-      return "<!-- " + command[1] + " -->\n<p>\n\t" + convertP(text) + "\n</p>\n";
+      return (
+        "<!-- " + command[1] + " -->\n<p>\n\t" + convertP(text) + "\n</p>\n"
+      );
     }
   }
 }
@@ -102,7 +104,7 @@ function convertBS(text: string) {
 function makeLines(text: string) {
   // Create an array of lines, with whitespace stripped from the beginning and end of each line.
   let lines = text.split(/\r\n|\r|\n/);
-  lines = lines.map(line => line.trim());
+  lines = lines.map((line) => line.trim());
   // console.log("LINES: "+lines );
   // console.log("LINES LENGTH: "+lines.length );
 
@@ -110,16 +112,21 @@ function makeLines(text: string) {
   if (lines.length > 1) {
     // Merge lines that start with regular characters into single lines.
     let i = 0;
-    while (i < lines.length-1) {
-      if (lines[i].length > 0 && lines[i+1].length > 0 && !lines[i].startsWith("\\") && !lines[i+1].startsWith("\\")) {
-        lines[i] = lines[i] + " " + lines[i+1];
-        lines.splice(i+1,1);
+    while (i < lines.length - 1) {
+      if (
+        lines[i].length > 0 &&
+        lines[i + 1].length > 0 &&
+        !lines[i].startsWith("\\") &&
+        !lines[i + 1].startsWith("\\")
+      ) {
+        lines[i] = lines[i] + " " + lines[i + 1];
+        lines.splice(i + 1, 1);
       } else {
         i += 1;
       }
     }
     // Then remove empty lines.
-    lines = lines.filter(line => line.length > 0);
+    lines = lines.filter((line) => line.length > 0);
   }
   // console.log("LINES: "+lines );
   // console.log("LINES LENGTH: "+lines.length );
@@ -138,10 +145,10 @@ function makeLines(text: string) {
   //       depth += 1;
   //       i += 1;
   //     } else if (lines[i].startsWith("\\end")) {
-  //       lines[i-1] = lines[i-1]+"\n"+lines[i]. 
+  //       lines[i-1] = lines[i-1]+"\n"+lines[i].
   //       depth -= 1;
   //     }
-      
+
   //   }
   //   if (convertCheck) {
   //     if (lines[i].trim().length === 0) {
@@ -212,30 +219,33 @@ function convertLines(lines: string[]) {
     //line is a \begin/\end: convert to pretext block
     else if (line.startsWith("\\begin")) {
       let env = line.match(/\\begin{(.*?)}/);
-      if (env !== null){
+      if (env !== null) {
         if (env[1] in BLOCKS) {
           result += "<" + BLOCKS[env[1]] + ">\n\t";
         } else {
-        result += "<!-- START " + env[1] + " environment (not defined in PreTeXt) -->\n\t";
+          result +=
+            "<!-- START " +
+            env[1] +
+            " environment (not defined in PreTeXt) -->\n\t";
         }
       }
     } else if (line.startsWith("\\end")) {
       let env = line.match(/\\end{(.*?)}/);
-      if (env !== null){
+      if (env !== null) {
         if (env[1] in BLOCKS) {
           result += "\n</" + BLOCKS[env[1]] + ">\n";
         } else {
-        result += "<!-- END " + env[1] + " environment -->\n";
+          result += "<!-- END " + env[1] + " environment -->\n";
         }
       }
     }
     //Line starts with a backslash command; could be something like \item
-    else if (line.startsWith("\\")){
-        result += convertBS(line);
-    } 
+    else if (line.startsWith("\\")) {
+      result += convertBS(line);
+    }
     // Otherwise we assume it is a paragraph.
     else {
-        result += "<p>\n\t" + convertP(line) + "\n</p>\n";
+      result += "<p>\n\t" + convertP(line) + "\n</p>\n";
     }
   }
   return result;
