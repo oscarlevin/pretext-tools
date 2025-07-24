02dcf729cf5587e2df1ba97c62c15e1c3a7f93f5
Oscar Levin <oscar.levin@unco.edu>
2025-07-24 15:59:27 -0700

    refactor

--- packages/vscode-extension/src/commands/convert.ts
+++ packages/vscode-extension/src/commands/convert.ts
@@ -35,62 +35,62 @@ export function cmdConvertFile() {
 
 
 export async function cmdConvertText() {
+  const editor = window.activeTextEditor;
+  if (!editor) {
+    pretextOutputChannel.appendLine("No active editor found to convert text.");
+    return;
+  }
+  const selection = editor.selection;
+  const selectionRange = selection.isEmpty
+    ? editor.document.lineAt(selection.start.line).range
+    : new Range(selection.start, selection.end);
+
+  const initialText = editor.document.getText(selectionRange);
+  let convertedText: string;
+
+  pretextOutputChannel.appendLine(
+    "Converting selected text to PreTeXt format."
+  );
   window.showQuickPick(["LaTeX", "Markdown", "Mixed text"], {
     placeHolder: "Which format is the selected text?",
-}).then((qpSelection) => {
+  }).then(async (qpSelection) => {
     if (!qpSelection) {
       return;
     }
     switch (qpSelection) {
       case "LaTeX":
-        cmdLatexToPretext();
+        convertedText = await cmdLatexToPretext(initialText, selectionRange);
         break;
       case "Markdown":
-        cmdMarkdownToPretext();
+        convertedText = await markdownToPretext(initialText);
         break;
       case "Mixed text":
-        cmdConvertMixedtextToPretext();
+        convertedText = await cmdConvertMixedtextToPretext(initialText);
         break;
     }
   })
+    .then(() => {
+      if (convertedText) {
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selectionRange, convertedText);
+        });
+      }
+    });
 }
 
 
-async function cmdLatexToPretext() {
-  const editor = window.activeTextEditor;
-  if (editor) {
-    const selection = editor.selection;
-    const selectionRange = selection.isEmpty
-      ? editor.document.lineAt(selection.start.line).range
-      : new Range(selection.start, selection.end);
+async function cmdLatexToPretext(initialText: string, selectionRange: Range) {
+  //var newText = latexToPretext(initialText);
+  let newText = convertWithUnified(initialText);
 
-    //console.log("selectionRange is", selectionRange);
-
-    const initialText = editor.document.getText(selectionRange);
-
-    //var newText = latexToPretext(initialText);
-    let newText = convertWithUnified(initialText);
-
-    // Remove the starting <p> tag if we selected text in the middle of a line.
-    const pTagMatch = newText.match(/^<p>/);
-    if (pTagMatch && selectionRange.start.character > 0) {
-      newText = newText.replace(/^<p>/, "");
-    }
-
-    // Split consecutive tags with a space if present before formatting.
-    const formattedNewText = await lspFormatText(newText.replace(/(>)(<)/g, "$1 $2"));
-
-    //The prettier formatter is finicky; needs correct structure to work?
-
-    //const formattedNewText = prettier.format(newText, {
-    //  parser: "ptx",
-    //  plugins: [prettierPluginPretext as unknown as prettier.Plugin],
-    //});
-
-    editor.edit((editbuilder) => {
-      editbuilder.replace(selectionRange, formattedNewText);
-    });
+  // Remove the starting <p> tag if we selected text in the middle of a line.
+  const pTagMatch = newText.match(/^<p>/);
+  if (pTagMatch && selectionRange.start.character > 0) {
+    newText = newText.replace(/^<p>/, "");
   }
+
+  // Split consecutive tags with a space if present before formatting.
+  return lspFormatText(newText.replace(/(>)(<)/g, "$1 $2"));
 }
 
 //const myMacroReplacements = {
@@ -146,60 +146,30 @@ function convertWithUnified(text: string) {
   return convert(text).value as string;
 }
 
-function cmdMarkdownToPretext() {
-  // Give warning that this function is currently unvailable.
-  pretextOutputChannel.appendLine(
-    "Markdown to PreTeXt conversion is still very experiemental.  Use with care."
-  );
-
-  const editor = window.activeTextEditor;
-  if (editor) {
-    const selection = editor.selection;
-    const selectionRange = selection.isEmpty
-      ? editor.document.lineAt(selection.start.line).range
-      : new Range(selection.start, selection.end);
-
-    const initialText = editor.document.getText(selectionRange);
-
-    var newText = markdownToPretext(initialText);
-
-    editor.edit((editbuilder) => {
-      editbuilder.replace(selectionRange, newText);
-    });
-  }
-}
-
-async function cmdConvertMixedtextToPretext() {
+async function cmdConvertMixedtextToPretext(initialText: string) {
   pretextOutputChannel.appendLine(
     "Mixed text to PreTeXt conversion is still very experiemental.  Use with care."
   );
-  const editor = window.activeTextEditor;
-  if (editor) {
-    const selection = editor.selection;
-    let selectionRange: Range;
-    let fullDocument = false;
-    if (selection.isEmpty) {
-      fullDocument = true;
-      selectionRange = new Range(
-        editor.document.positionAt(0),
-        editor.document.positionAt(editor.document.getText().length)
-      );
-    } else {
-      selectionRange = new Range(selection.start, selection.end);
-    }
-    const initialText = editor.document.getText(selectionRange);
-    const newText = FlexTeXtConvert(initialText);
-    const formattedNewText = await lspFormatText(newText);
-    editor.edit((editbuilder) => {
-      editbuilder.replace(selectionRange, formattedNewText);
-    });
-    pretextOutputChannel.appendLine("Mixed text converted to PreTeXt.");
-  }
+  const newText = FlexTeXtConvert(initialText);
+  return lspFormatText(newText);
 }
 
 ////////////////// Experiments /////////////////////
 
 export async function cmdExperimentConvert() {
+  const editor = window.activeTextEditor;
+  if (!editor) {
+    pretextOutputChannel.appendLine("No active editor found to convert text.");
+    return;
+  }
+  const selection = editor.selection;
+  const selectionRange = selection.isEmpty
+    ? editor.document.lineAt(selection.start.line).range
+    : new Range(selection.start, selection.end);
+
+  const initialText = editor.document.getText(selectionRange);
+  let convertedText: string;
+
   // Prompt user to select a conversion method
   pretextOutputChannel.appendLine(
     "Experimental conversion functions are designed for testing and may not work as expected."
@@ -209,143 +179,89 @@ export async function cmdExperimentConvert() {
       {
         label: "Use mdast",
         description: "Preprocess selected text with Markdown AST",
-        function: "convertMixedTextWithMdast",
+        function: "useMdast",
       },
       {
         label: "Use xast",
         description: "Convert selected text to PreTeXt format",
-        function: "convertMixedTextWithXast"
+        function: "useXast"
       },
-      {
-        label: "Use FrankenMarkup",
-        description: "Convert selected text to PreTeXt format",
-        function: "convertMixedTextWithFrankenMarkup"
-      }
     ]
-  ).then((selection) => {
+  ).then(async (selection) => {
     if (!selection) {
       return;
     }
     switch (selection.function) {
-      case "convertMixedTextWithMdast":
-        cmdConvertPmdWithMdast();
+      case "useMdast":
+        convertedText = await convertPmdWithMdast(initialText);
         break;
-      case "convertMixedTextWithXast":
-        cmdConvertMixedTextToPretextViaXast();
+      case "useXast":
+        convertedText = await convertPmdWithXast(initialText);
         break;
-      case "convertMixedTextWithFrankenMarkup":
-        cmdConvertMixedtextToPretext();
-        break;
+    }
+  }).then(() => {
+    if (convertedText) {
+      editor.edit((editbuilder) => {
+        editbuilder.replace(selectionRange, convertedText);
+      });
     }
   });
 }
 
-async function cmdConvertPmdWithMdast() {
-  pretextOutputChannel.appendLine(
-    "Mixed text to PreTeXt conversion is still very experiemental.  Use with care."
-  );
-  const editor = window.activeTextEditor;
-  if (editor) {
-    const selection = editor.selection;
-    let selectionRange: Range;
-    let fullDocument = false;
-    if (selection.isEmpty) {
-      fullDocument = true;
-      selectionRange = new Range(
-        editor.document.positionAt(0),
-        editor.document.positionAt(editor.document.getText().length)
-      );
-    } else {
-      selectionRange = new Range(selection.start, selection.end);
-    }
-    const initialText = editor.document.getText(selectionRange);
-    // Remove leading and trailing whitespace
-    const trimmedText = initialText.trim();
-    console.log("initialText is", trimmedText);
+async function convertPmdWithMdast(initialText: string) {
+  // Remove leading and trailing whitespace
+  const trimmedText = initialText.trim();
+  console.log("initialText is", trimmedText);
 
-    // Converte to mdast:
-    const tree = fromMarkdown(trimmedText);
+  // Converte to mdast:
+  const tree = fromMarkdown(trimmedText);
 
-    console.log("mdast before: ", tree);
+  console.log("mdast before: ", tree);
 
-    //const convertedText = toXml(tree);
+  //const convertedText = toXml(tree);
 
-    // Convert the text using FlexTeXtConvert
-    //const convertedText = FlexTeXtConvert(trimmedText);
-    const convertedText = initialText;
-    console.log("converted text is", convertedText);
+  // Convert the text using FlexTeXtConvert
+  //const convertedText = FlexTeXtConvert(trimmedText);
+  const convertedText = initialText;
+  console.log("converted text is", convertedText);
 
-    // Format the converted text
-    const formattedNewText = await lspFormatText(convertedText);
-
-    editor.edit((editbuilder) => {
-      editbuilder.replace(selectionRange, formattedNewText);
-    });
-    pretextOutputChannel.appendLine("Mixed text converted to PreTeXt.");
-  }
-
+  // Format the converted text
+  return lspFormatText(convertedText);
 }
 
 
-async function cmdConvertMixedTextToPretextViaXast() {
-  console.log("cmdConvertMixedTextToPretextViaXast called");
-  pretextOutputChannel.appendLine(
-    "Mixed text to PreTeXt conversion is still very experiemental.  Use with care."
-  );
-  const editor = window.activeTextEditor;
-  if (editor) {
-    const selection = editor.selection;
-    let selectionRange: Range;
-    let fullDocument = false;
-    if (selection.isEmpty) {
-      fullDocument = true;
-      selectionRange = new Range(
-        editor.document.positionAt(0),
-        editor.document.positionAt(editor.document.getText().length)
-      );
-    } else {
-      selectionRange = new Range(selection.start, selection.end);
+async function convertPmdWithXast(initialText: string) {
+  console.log("initialText is", initialText);
+
+  const tree = fromXml(`<root>${initialText}</root>`);
+
+  console.log("xast before: ", tree);
+
+  visit(tree, (node, index, parent) => {
+    if (node.type === "text") {
+      const converted = FlexTeXtConvert(node.value);
+
+      console.log("converted text is", converted);
+
+      const subtree = fromXml(converted);
+      // replace the node with the subtree
+      if (typeof index !== "number" || !parent) return;
+
+      parent.children.splice(index, 1, ...subtree.children);
+      return SKIP;
     }
-    let initialText = editor.document.getText(selectionRange);
-    // Remove leading and trailing whitespace
-    initialText = initialText.trim();
-    console.log("initialText is", initialText);
+  });
 
-    const tree = fromXml(`<root>${initialText}</root>`);
+  console.log("xast after: ", tree);
+  // Convert the resulting tree back to XML
+  let newXml = toXml(tree);
 
-    console.log("xast before: ", tree);
-
-    visit(tree, (node, index, parent) => {
-      if (node.type === "text") {
-        const converted = FlexTeXtConvert(node.value);
-
-        console.log("converted text is", converted);
-
-        const subtree = fromXml(converted);
-        // replace the node with the subtree
-        if (typeof index !== "number" || !parent) return;
-
-        parent.children.splice(index, 1, ...subtree.children);
-        return SKIP;
-      }
-    });
-
-    console.log("xast after: ", tree);
-    // Convert the resulting tree back to XML
-    let newXml = toXml(tree);
-
-    console.log("back to xml: ", newXml);
-    // strip the <root> and </root> tags
-    const rootTagMatch = newXml.match(/^<root>\s*([\s\S]*?)\s*<\/root>$/);
-    if (rootTagMatch) {
+  console.log("back to xml: ", newXml);
+  // strip the <root> and </root> tags
+  const rootTagMatch = newXml.match(/^<root>\s*([\s\S]*?)\s*<\/root>$/);
+  if (rootTagMatch) {
     newXml = rootTagMatch[1];
-    }
-
-    const formattedNewText = await lspFormatText(newXml);
-
-    editor.edit((editbuilder) => {
-      editbuilder.replace(selectionRange, formattedNewText);
-    });
-    pretextOutputChannel.appendLine("Mixed text converted to PreTeXt.");
   }
+
+  return lspFormatText(newXml);
 }
